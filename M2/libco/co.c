c4bf0da65b6671b39c942b72b49f108dbe2e2313
tracer-nju <tracer@nju.edu.cn>
2024-12-19 22:20:37 +0800

    Linux cd2c2g 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:20:37 up 35 days,  1:01,  1 user,  load average: 0.00, 0.02, 0.00

--- M2/libco/co.c
+++ M2/libco/co.c
@@ -63,7 +63,8 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
      movq %%rsp,  %%rcx; movq %%rcx, 32(%0); \
      movq %0,  %%rsp; \
      movq %2, %%rdi; \
-     call *%1;"
+     call *%1; \
+     movq 32(%0),%%rcx; movq %%rcx, %%rsp;"
       : : "b"((uintptr_t)sp), "d"(entry), "a"(arg)  : "memory"
 #else
     "movl 8(%%esp),  %%ecx; movl %%ecx, (%0); \
