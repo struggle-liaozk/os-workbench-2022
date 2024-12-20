fb9a40408068bcf136778f85b5a4775791005733
tracer-nju <tracer@nju.edu.cn>
2024-12-20 16:55:53 +0800

    Linux cd2c2g 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:55:53 up 35 days, 19:36,  1 user,  load average: 0.06, 0.06, 0.01

--- M2/libco/co.c
+++ M2/libco/co.c
@@ -173,7 +173,6 @@ void co_yield() {
       debug("co_new return %s \n", "a");
       if (next -> waiter) {
         next -> waiter -> status = CO_RUNNING;
-        co_yield();
       }
       break; 
     case CO_RUNNING:
