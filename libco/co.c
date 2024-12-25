b04ff3840027a45dfd5fbffa54572a0f3afeac6c
tracer-nju <tracer@nju.edu.cn>
2024-12-25 23:56:00 +0800

    Linux cd2c2g 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:56:00 up 1 day,  8:33,  1 user,  load average: 0.05, 0.01, 0.00

--- libco/co.c
+++ libco/co.c
@@ -221,7 +221,7 @@ void co_yield() {
       current -> status = CO_RUNNING;
       stack_switch_call((current -> stack + STACK_SIZE - 16), current -> func, (uintptr_t)(current -> arg));
       debug("return stcak_switch %s \n", current -> name);
-      //restore_return((current -> stack + STACK_SIZE - 16));
+      restore_return((current -> stack + STACK_SIZE - 16));
       //debug("return restore_return %s \n", current -> name);
       current -> status = CO_DEAD;
       if (current -> waiter) {
