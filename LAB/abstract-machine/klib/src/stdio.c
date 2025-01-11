df24d4bc1c0a74f9e05a4c5208b30f477e2904b6
liaozk <zikai.liao@outlook.com>
2025-01-11 19:58:13 +0800

    printf 的测试与试验

--- LAB/abstract-machine/klib/src/stdio.c
+++ LAB/abstract-machine/klib/src/stdio.c
@@ -14,95 +14,50 @@ int printf(const char *fmt, ...) {
   vsnprintf(buffer, 1024, fmt, args);
 
   putstr(buffer);
+  va_end(args);
   return 1;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  char* start = out;
-  while (fmt != '\0') {
-    if (fmt == '%') {
-      fmt++;
-      switch (*fmt)
-      {
-      case 's':
-        char *replace = va_arg(ap, char*);
-        while (replace != '\0') {
-          *out = *replace;
-          out++;
-          replace++;
-        }
-        break;
-      case 'd':
-        int d = va_arg(ap, int); 
-        if (d != 0) {
-          char a[32];
-          int i_a = 0;
-          while (d != 0) {
-            char b = 48 + (d % 10);
-            a[i_a] = b;
-            d = d / 10;
-            i_a ++;
-          }
-          while (i_a > 0) {
-            *out = a[i_a];
-            out++;
-            i_a--;
-          }
-        } else {
-          *out = 48;
-        }
-        break;
-      default:
-        panic("Not implemented this case");
-        break;
-      }
-    } else {
-      *out = *fmt;
-    }
-    fmt++;
-    out++;
-  }
-  
-  *out = '\0';
-  return out - start;
+  panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
 
-  return vsprintf(out, fmt, args);
+  int rs = vsprintf(out, fmt, args);
+  va_end(args);
+  return rs;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
 
-  return vsnprintf(out, n, fmt, args);
+  int rs = vsnprintf(out, n, fmt, args);
+  va_end(args);
+  return rs;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  size_t num = 0;
-  while (fmt != '\0') {
-    if (fmt == '%') {
+  size_t len = 0;
+  while (*fmt != '\0' && len < n) {
+    if (*fmt == '%') {
       fmt++;
       switch (*fmt)
       {
       case 's':
         char *replace = va_arg(ap, char*);
-        while (replace != '\0') {
+        while (*replace != '\0') {
           *out = *replace;
           out++;
-          num++;
-          if (num >= n) {
-            *out = '\0';
-            return num;
-          }
+          len++;
           replace++;
         }
         break;
       case 'd':
-        int d = va_arg(ap, int); 
+        int d = va_arg(ap, int);
         if (d != 0) {
           char a[32];
           int i_a = 0;
@@ -112,18 +67,72 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
             d = d / 10;
             i_a ++;
           }
-          while (i_a > 0) {
+          i_a--;
+          while (i_a >= 0) {
             *out = a[i_a];
             out++;
-            num++;
-            if (num >= n) {
-            *out = '\0';
-            return num;
-            }
+            len++;
             i_a--;
           }
         } else {
           *out = 48;
+          out++;
+          len++;
+        }
+        break;
+      case 'p':
+        *out = 48; out ++;
+        *out = 'x';out ++;
+        long p = va_arg(ap, long);
+        if (p != 0) {
+          char a[64];
+          int i_p = 0;
+          while (p != 0) {
+            int r = p % 16;
+            char h;
+            if (r < 10) {
+              h = 48 + r;
+            } else {
+              switch (r)
+              {
+              case 10:
+                h = 'a';
+                break;
+              case 11:
+                h = 'b';
+                break;
+              case 12:
+                h = 'c';
+                break;
+              case 13:
+                h = 'd';
+                break;
+              case 14:
+                h = 'e';
+                break;
+              case 15:
+                h = 'f';
+                break;
+              default:
+                break;
+              }
+            }
+
+            a[i_p] = h;
+            p = p / 16;
+            i_p ++;
+          }
+          i_p--;
+          while (i_p >= 0) {
+            *out = a[i_p];
+            out++;
+            len++;
+            i_p--;
+          }
+        } else {
+          *out = 48;
+          out++;
+          len++;
         }
         break;
       default:
@@ -132,26 +141,14 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
       }
     } else {
       *out = *fmt;
+      out++;
+      len++;
     }
     fmt++;
-    out++;
-    num++;
-    if (num >= n) {
-        *out = '\0';
-        return num;
-    }
   }
-  if (num > 0) {
-    *out = '\0';
-  }
-  return num;
+  
+  *out = '\0';
+  return len;
 }
 
 #endif
-
-
-int main() {
-  printf("test str= %s \n", "this is string");
-
-  printf("test num= %d \n", 1024);
-}
--- LAB/kernel/src/pmm.c
+++ LAB/kernel/src/pmm.c
@@ -11,15 +11,19 @@ static void kfree(void *ptr) {
 // 框架代码中的 pmm_init (在 AbstractMachine 中运行)
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
+  printf("test printf str= %s abc\n", "right?");
+  printf("test printf int= %d def\n", 2048);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  printf("test printf ptr= %p \n", 255);
 }
 #else
+#define HEAP_SIZE 125 * 1024 * 1024
 // 测试代码的 pmm_init ()
 static void pmm_init() {
   char *ptr  = malloc(HEAP_SIZE);
   heap.start = ptr;
   heap.end   = ptr + HEAP_SIZE;
-  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+  printf("Got %d MiB heap: [%d, %d)\n", HEAP_SIZE >> 20, heap.start, heap.end);
 }
 #endif
 
